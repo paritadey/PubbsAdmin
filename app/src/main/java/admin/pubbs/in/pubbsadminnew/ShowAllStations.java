3288f7773b1e821984066a6382cb5028af6db6a8
Parita Dey <paritasampa95@gmail.com>
2018-10-22 09:55:43 +0530

    Super admin can view all station lists--parita

--- app/src/main/java/admin/pubbs/in/pubbsadminnew/ShowAllStations.java
+++ app/src/main/java/admin/pubbs/in/pubbsadminnew/ShowAllStations.java
@@ -1,13 +1,169 @@
 package admin.pubbs.in.pubbsadminnew;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class ShowAllStations extends AppCompatActivity {
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowAllStations extends AppCompatActivity implements AsyncResponse{
+    ImageView back;
+    private TextView addOperatorTv;
+    EditText inputSearch;
+    ProgressBar circularProgressbar;
+    private RecyclerView recyclerView;
+    private AllStationAdapter allStationAdapter;
+    private List<StationList> stationLists = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_all_stations);
+        Typeface type1 = Typeface.createFromAsset(getAssets(), "fonts/AvenirLTStd-Book.otf");
+        Typeface type2 = Typeface.createFromAsset(getAssets(), "fonts/AvenirNextLTPro-Medium.otf");
+        Typeface type3 = Typeface.createFromAsset(getAssets(), "fonts/AvenirNextLTPro-Bold.otf");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        back = findViewById(R.id.back_button);
+        addOperatorTv = findViewById(R.id.add_operator_tv);
+        addOperatorTv.setTypeface(type1);
+        inputSearch = findViewById(R.id.input_search);
+        inputSearch.setTypeface(type1);
+        circularProgressbar = findViewById(R.id.circularProgressbar);
+        recyclerView = findViewById(R.id.recycler_view);
+        allStationAdapter = new AllStationAdapter(stationLists);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new CustomDivider(this, LinearLayoutManager.VERTICAL, 8));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(allStationAdapter);
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                StationList lists = stationLists.get(position);
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+            }
+        }));
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ShowAllStations.this, DashBoardActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadData();
+    }
+
+    private void loadData() {
+        circularProgressbar.setVisibility(View.VISIBLE);
+        JSONObject jo = new JSONObject();
+        try {
+            jo.put("method", "getalladminstations");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        new SendRequest(getResources().getString(R.string.url), jo, ShowAllStations.this,
+                getApplicationContext()).executeJsonRequest();
+    }
+
+    @Override
+    public void onResponse(JSONObject jsonObject) {
+        circularProgressbar.setVisibility(View.GONE);
+        stationLists.clear();
+        if (jsonObject.has("method")) {
+            try {
+                if (jsonObject.getString("method").equals("getalladminstations") && jsonObject.getBoolean("success")) {
+                    JSONArray ja = jsonObject.getJSONArray("data");
+                    if (ja.length() > 0) {
+                        for (int i = 0; i < ja.length(); i++) {
+                            JSONObject jo = ja.getJSONObject(i);
+                            StationList list = new StationList(jo.getString("full_name"),
+                                    jo.getString("adminmobile"), jo.getString("station_name"), jo.getString("station_id"),
+                                    jo.getString("area_name"));
+                            stationLists.add(list);
+                        }
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        allStationAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onResponseError(VolleyError error) {
+        showDialog("Server Error !");
+    }
+    private void showDialog(String message) {
+        Typeface type1 = Typeface.createFromAsset(getAssets(), "fonts/AvenirLTStd-Book.otf");
+        Typeface type2 = Typeface.createFromAsset(getAssets(), "fonts/AvenirNextLTPro-Bold.otf");
+
+        final AlertDialog dialogBuilder = new AlertDialog.Builder(this).create();
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.custom_alert_dialog, null);
+
+        final TextView serverProblem = (TextView) dialogView.findViewById(R.id.server_problem);
+        final TextView extraLine = (TextView) dialogView.findViewById(R.id.extra_line);
+        extraLine.setTypeface(type1);
+        serverProblem.setTypeface(type1);
+        serverProblem.setText(message);
+        Button ok = (Button) dialogView.findViewById(R.id.ok_btn);
+        ok.setTypeface(type2);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogBuilder.dismiss();
+                if (circularProgressbar.isEnabled()) {
+                    circularProgressbar.setVisibility(View.GONE);
+                }
+                Intent intent = new Intent(ShowAllStations.this, DashBoardActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+        dialogBuilder.setView(dialogView);
+        dialogBuilder.show();
+        dialogBuilder.setCancelable(false);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(ShowAllStations.this, DashBoardActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 }
