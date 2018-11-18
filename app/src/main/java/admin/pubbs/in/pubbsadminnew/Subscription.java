5cece0c6f542c6fdaff6f7a1fa495800f7d49583
paritadey <paritasampa95@gmail.com>
2018-11-18 12:35:56 +0530

    Subscription design is done and checked--parita

--- app/src/main/java/admin/pubbs/in/pubbsadminnew/Subscription.java
+++ app/src/main/java/admin/pubbs/in/pubbsadminnew/Subscription.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -52,38 +53,66 @@ public class Subscription extends AppCompatActivity {
                 "-" + sweet_plan_mins + "-" + premium_plan_amount + "-" + premium_plan_month + "-" + premium_plan_mins);
         area_name_tv = findViewById(R.id.area_name);
         area_name_tv.setText(area_name);
+        area_name_tv.setTypeface(type2);
         area_id_tv = findViewById(R.id.area_id);
         area_id_tv.setText(area_id);
+        area_id_tv.setTypeface(type1);
         basic_plan_name = findViewById(R.id.basic_plan_name);
+        basic_plan_name.setTypeface(type3);
         basic_plan_amount_tv = findViewById(R.id.basic_plan_amount);
-        basic_plan_amount_tv.setText(basic_plan_amount);
+        basic_plan_amount_tv.setText("Amount : "+""+basic_plan_amount);
+        basic_plan_amount_tv.setTypeface(type1);
         basic_plan_month_tv = findViewById(R.id.basic_plan_month);
-        basic_plan_month_tv.setText(basic_plan_month);
+        basic_plan_month_tv.setText("Month : "+""+basic_plan_month);
+        basic_plan_month_tv.setTypeface(type1);
         basic_plan_mins_tv = findViewById(R.id.basic_plan_mins);
-        basic_plan_mins_tv.setText(basic_plan_mins);
+        basic_plan_mins_tv.setText("Mins to be added : "+""+basic_plan_mins);
+        basic_plan_mins_tv.setTypeface(type1);
         standard_plan_name = findViewById(R.id.standard_plan_name);
+        standard_plan_name.setTypeface(type3);
         standard_plan_amount_tv = findViewById(R.id.standard_plan_amount);
-        standard_plan_amount_tv.setText(standard_plan_amount);
+        standard_plan_amount_tv.setText("Amount : "+""+standard_plan_amount);
+        standard_plan_amount_tv.setTypeface(type1);
         standard_plan_month_tv = findViewById(R.id.standard_plan_month);
-        standard_plan_month_tv.setText(standard_plan_month);
+        standard_plan_month_tv.setText("Month : "+""+standard_plan_month);
+        standard_plan_month_tv.setTypeface(type1);
         standard_plan_mins_tv = findViewById(R.id.standard_plan_mins);
-        standard_plan_mins_tv.setText(standard_plan_mins);
+        standard_plan_mins_tv.setText("Mins to be added : "+""+standard_plan_mins);
+        standard_plan_mins_tv.setTypeface(type1);
         sweet_plan_name = findViewById(R.id.sweet_plan_name);
+        sweet_plan_name.setTypeface(type3);
         sweet_plan_amount_tv = findViewById(R.id.sweet_plan_amount);
-        sweet_plan_amount_tv.setText(sweet_plan_amount);
+        sweet_plan_amount_tv.setText("Amount : "+""+sweet_plan_amount);
+        sweet_plan_amount_tv.setTypeface(type1);
         sweet_plan_month_tv = findViewById(R.id.sweet_plan_month);
-        sweet_plan_month_tv.setText(sweet_plan_month);
+        sweet_plan_month_tv.setText("Month : "+""+sweet_plan_month);
+        sweet_plan_month_tv.setTypeface(type1);
         sweet_plan_mins_tv = findViewById(R.id.sweet_plan_mins);
-        sweet_plan_mins_tv.setText(sweet_plan_mins);
+        sweet_plan_mins_tv.setText("Mins to be added : "+""+sweet_plan_mins);
+        sweet_plan_mins_tv.setTypeface(type1);
         premium_plan_name = findViewById(R.id.premium_plan_name);
+        premium_plan_name.setTypeface(type3);
         premium_plan_amount_tv = findViewById(R.id.premium_plan_amount);
-        premium_plan_amount_tv.setText(premium_plan_amount);
+        premium_plan_amount_tv.setText("Amount : "+""+premium_plan_amount);
+        premium_plan_amount_tv.setTypeface(type1);
         premium_plan_month_tv = findViewById(R.id.premium_plan_month);
-        premium_plan_month_tv.setText(premium_plan_month);
+        premium_plan_month_tv.setText("Month : "+""+premium_plan_month);
+        premium_plan_month_tv.setTypeface(type1);
         premium_plan_mins_tv = findViewById(R.id.premium_plan_mins);
-        premium_plan_mins_tv.setText(premium_plan_mins);
+        premium_plan_mins_tv.setText("Mins to be added : "+""+premium_plan_mins);
+        premium_plan_mins_tv.setTypeface(type1);
         subscription_tv = findViewById(R.id.subscription_tv);
+        subscription_tv.setTypeface(type1);
         back = findViewById(R.id.back_button);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Subscription.this, AreaSubscriptionHistory.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
